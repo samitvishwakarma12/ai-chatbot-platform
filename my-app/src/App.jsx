0d22762ac2a5b6545d89e045799b97665291c195
samitvishwakarma <samitvishwakarma.edu@gmail.com>
2026-06-09 17:43:02 +0000

    fix(MessageBox): fix messages get overlapped by promptbox

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,18 +1,19 @@
 import { useState } from "react";
 import PromptBox from "./components/PromptBox";
 import MessageBox from "./components/MessageBox";
+import "./App.css"
 
 function App() {
   const [messages, setMessages] = useState([]);
 
   return (
-    <>
+    <div className="app">
       <PromptBox
         messages={messages}
         setMessages={setMessages}
       />
       <MessageBox messages={messages} />
-    </>
+    </div>
   );
 }
 
