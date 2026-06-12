a17f356788d8c75a7ca7702195f84892a054ade6
samitvishwakarma <samitvishwakarma.edu@gmail.com>
2026-06-12 04:22:28 +0000

    style(ui): Improve page rendering animation

--- my-app/src/chat/PromptBox.jsx
+++ my-app/src/chat/PromptBox.jsx
@@ -24,7 +24,7 @@ function PromptBox({ setMessages, setLoading, setEmptyChatState }){
       <textarea
         value={prompt}
         onChange={(e) => setPrompt(e.target.value)}
-        placeholder="  Your prompt goes here..."
+        placeholder="Your prompt goes here..."
       />
 
       <button onClick={async () => handlePromptSubmit()}>
